e3eb09d989d16daa986202b6049aaf3c230f072f
Anna Kinds <anna.kinds@student.howest.be>
2025-01-22 15:11:26 +0100

    css

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
-import './style.css';
 import './reset.css';
+import './style.css';
 
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+const drawHeroimage = () => {
+  gsap.to(".heroimg__photo2", {
+    x: -220,
+    willChange: "transform",
+    scrollTrigger: {
+      trigger: "body",
+      pin: "body",
+      start: "top top",
+      end: "10% top",
+      // markers: true,
+      scrub: true,
+    },
+  });
+}
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const printingPress = () => {
+  const step = document.querySelector(".section3__list-item");
 
-// setupCounter(document.querySelector('#counter'))
+}
+
+const init = () => {
+  drawHeroimage();
+  printingPress();
+}
+init();
