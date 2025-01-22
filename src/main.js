0375411136ea094e9d015eb1acb130801192f16c
Anna Kinds <anna.kinds@student.howest.be>
2025-01-22 22:19:05 +0100

    feedback printing press interaction

--- src/main.js
+++ src/main.js
@@ -17,11 +17,36 @@ const drawHeroimage = () => {
   });
 }
 
+const step = document.getElementById("section3__list");
+
+const feedbackPrintingPress = () => {
+  const correctOrder = [
+    "letters kiezen",
+    "tekst vormen",
+    "tekst vast zetten",
+    "inkt op tekst",
+    "tekst op machine plaatsen",
+    "persen met machine"
+  ];
+
+  correctOrder.forEach((text, index) => {
+    if (step.children[index].textContent.trim() === text) {
+      step.children[index].classList.remove('wrong'); 
+      step.children[index].classList.add('right'); 
+    } else {
+      step.children[index].classList.remove('right'); 
+      step.children[index].classList.add('wrong'); 
+    }
+  });
+};
+
 const printingPress = () => {
-  const step = document.getElementById("section3__list");
   Sortable.create(step, {
     group: 'section3__list',
     animation: 150,
+    onEnd: () => {
+      feedbackPrintingPress(); 
+    },
   });
 }
 
